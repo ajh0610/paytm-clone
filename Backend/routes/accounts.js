8659fab81ce8a692e7184c13d76c8ff9c4fbdd79
ArjunHurket <arjunhurkat@gmail.com>
2024-08-09 01:31:28 +0530

    Using without transactions

--- Backend/routes/accounts.js
+++ Backend/routes/accounts.js
@@ -20,39 +20,28 @@ router.get('/balance', authmiddleware, async (req, res) => {
 })
 
 router.post('/transfer', authmiddleware, async (req, res)=>{
-   
-    const session = await mongoose.startSession();
-    session.startTransaction();
 
-    try{
-    
-        const {to, amount} = req.body;
-    
-        const fromAccount = await Accounts.findOne({userId: req.userId}).session(session);
-    
-        if(!fromAccount || fromAccount.balance < amount){
-            return res.status(400).json({mssg: "Insufficient Balance"});
-        }
-    
-    
-        const toAccount = await Accounts.findOne({userId: to}).session(session);
-    
-        if(!toAccount){
-            return res.status(400).json({mssg: "Invalid to account"});
-        }
-    
-        await Accounts.updateOne({userId: req.userId}, {$inc: {balance: -amount}}).session(session);
-        await Accounts.updateOne({userId: to}, {$inc: {balance: amount}}).session(session);
-    
-        await session.commitTransaction();
-    
-        res.status(200).json({mssg: "Transfer was Succesfull!"});
-    } catch(e){
-        console.log(e);
-        await session.abortTransaction();
-        res.status(500).json({mssg: "Error while doing transaction!"})
+
+    const {to, amount} = req.body;
+
+    const fromAccount = await Accounts.findOne({userId: req.userId});
+
+    if(!fromAccount || fromAccount.balance < amount){
+        return res.status(400).json({mssg: "Insufficient Balance"});
     }
 
+
+    const toAccount = await Accounts.findOne({userId: to});
+
+    if(!toAccount){
+        return res.status(400).json({mssg: "Invalid to account"});
+    }
+
+    await Accounts.updateOne({userId: req.userId}, {$inc: {balance: -amount}});
+    await Accounts.updateOne({userId: to}, {$inc: {balance: amount}});
+    
+    res.status(200).json({mssg: "Transfer was Succesfull!"});
+    
 })
 
 
